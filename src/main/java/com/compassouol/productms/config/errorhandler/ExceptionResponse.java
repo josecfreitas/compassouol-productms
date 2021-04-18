fb6e3430769839dc5078f3ddf51794365d79b924
José Carlos de Freitas <jcfreitas2000@hotmail.com>
2021-04-18 18:22:29 -0300

    adding snake case jackson configuration

--- src/main/java/com/compassouol/productms/config/errorhandler/ExceptionResponse.java
+++ src/main/java/com/compassouol/productms/config/errorhandler/ExceptionResponse.java
@@ -1,6 +1,5 @@
 package com.compassouol.productms.config.errorhandler;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,7 +7,6 @@ import lombok.Getter;
 @Getter
 public class ExceptionResponse {
 
-    @JsonProperty("status_code")
     private final int statusCode;
     private final String message;
 }
